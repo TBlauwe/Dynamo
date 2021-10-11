9fe227bd14c559eec1d24c381e0994052068547c
Tristan de Blauwe <tristan.de-blauwe@etu.utc.fr>
2021-10-11 12:52:08 +0200

    Documenting

--- dynamo/include/dynamo/dynamo.hpp
+++ dynamo/include/dynamo/dynamo.hpp
@@ -5,14 +5,34 @@
 #include <dynamo/modules/basic_perception.hpp>
 
 namespace dynamo{
+
+    /**
+     * Simulation of a number of agents evolving inside a world.
+     */
     class Simulation{
-        flecs::world world {};
+        flecs::world _world {};
 
     public:
+        /**
+         * Create an empty simulation.
+         */
         Simulation();
 
+        /**
+         * Advance simulation by one-step.
+         */
         void step();
+
+        /**
+         * Advance simulation by n-step.
+         * @param n number of steps
+         */
         void step_n(unsigned int n = 0);
+
+        /**
+         * @return world - an ecs "database".
+         */
+        flecs::world& world();
     };
 }//namespace dynamo
 
--- dynamo/src/dynamo.cpp
+++ dynamo/src/dynamo.cpp
@@ -1,16 +1,20 @@
 #include <dynamo/dynamo.hpp>
 
 dynamo::Simulation::Simulation() {
-    world.import<module::Core>();
-    world.import<module::GlobalPerception>();
+    _world.import<module::Core>();
+    _world.import<module::GlobalPerception>();
 }
 
 void dynamo::Simulation::step(){
-    world.progress();
+    _world.progress();
 }
 
 void dynamo::Simulation::step_n(unsigned int n) {
-    for(int i {0}; i < n; i++){
+    for (int i{0}; i < n; i++) {
         step();
     }
+}
+
+flecs::world &dynamo::Simulation::world() {
+    return _world;
 }
