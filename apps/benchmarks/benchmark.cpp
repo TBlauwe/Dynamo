714d2530dd68937b0d97c15cf0948dde5b73c18c
Tristan de Blauwe <tristan.de-blauwe@etu.utc.fr>
2021-10-11 10:59:01 +0200

    Added simple benchmark (empty simulation)

--- apps/benchmarks/benchmark.cpp
+++ apps/benchmarks/benchmark.cpp
@@ -1,13 +1,14 @@
 #include <benchmark/benchmark.h>
 #include <dynamo/dynamo.hpp>
 
-static void BM_endl(benchmark::State& state) {
+static void BM_creating_simulation_empty(benchmark::State& state) {
     for (auto _ : state) {
         flecs::world world{};
-        //world.import<dynamo::Simulation>();
+        world.import<dynamo::Simulation>();
     }
 }
-BENCHMARK(BM_endl);
+
+BENCHMARK(BM_creating_simulation_empty);
 
 // Run the benchmark
 BENCHMARK_MAIN();
